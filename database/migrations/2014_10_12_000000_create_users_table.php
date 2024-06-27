b489b0e652011c8f28c9b432030e8206170cbcdd
rin.notepc <nekosuki.works@gmail.com>
2024-06-27 14:55:02 +0900

    user周りのデータベース設計を変更

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('position_id')->constrained();
+            $table->foreignId('grade_id')->constrained();
+            $table->foreignId('department_id')->constrained();
         });
     }
 
