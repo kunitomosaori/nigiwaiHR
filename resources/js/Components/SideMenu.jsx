35986ba829364d1154ab678cbe82882205f43619
rin.notepc <nekosuki.works@gmail.com>
2024-06-28 14:35:55 +0900

    SideMenuのデザインを変更。メニューのツリー構造を実装。
    子要素を設定してあるメニューにはトグルボタンが表示されるようにし、
    トグルボタンを押すと子要素に設定したメニューが展開されるようにした。

--- resources/js/Components/SideMenu.jsx
+++ resources/js/Components/SideMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "@inertiajs/react";
-import { TbInbox, TbChevronDown, TbChevronRight } from "react-icons/tb";
+import { TbInbox, TbChevronDown, TbChevronRight, TbPencilPlus, TbCircleDotted } from "react-icons/tb";
 const treeData = [
     {
         title: '受信トレイ',
@@ -28,22 +28,25 @@ const treeData = [
     {
         title: '評価シート作成',
         key: '0-0',
-        icon: <TbInbox className="text-2xl" />,
+        icon: <TbPencilPlus className="text-2xl" />,
         link: '/dashboard',
     },
     {
         title: '期初',
         key: '0-1',
+        icon: <TbCircleDotted className="text-2xl" />,
         link: '/idl/index_idl',
     },
     {
         title: '期末',
         key: '0-2',
+        icon: <TbCircleDotted className="text-2xl" />,
         link: '/idl/select_idl',
     },
     {
         title: 'ログアウト',
         key: '0-3',
+        icon: <TbCircleDotted className="text-2xl" />,
         link: '/phpkadai05/logout',
     },
 ];
@@ -53,12 +56,14 @@ const TreeNode = ({ node }) => {
         setExpanded(!expanded);
     };
     return (
-        <li className="border-b md:border-none flex flex-col hover:bg-sky-700 rounded">
-            <div className="flex items-center">
-                {node.children && (
+        <li className="border-b md:border-none flex flex-col">
+            <div className="flex items-center hover:bg-sky-700 rounded">
+                {node.children ? (
                     <span onClick={handleExpand} className="cursor-pointer">
-                        {expanded ? <TbChevronDown className="text-2xl" /> : <TbChevronRight className="text-2xl" />}
+                        {expanded ? <TbChevronDown className="text-2xl w-5" /> : <TbChevronRight className="text-2xl w-5" />}
                     </span>
+                ) : (
+                    <span className="w-5"></span> // トグルボタンと同じ幅のpaddingを追加
                 )}
                 {node.icon && node.icon}
                 <Link href={node.link} className="block px-6 py-1 my-2">
@@ -78,7 +83,7 @@ const TreeNode = ({ node }) => {
 const SideMenu = ({ menuVisible }) => {
     return (
         <div
-            className={`bg-sky-950 w-1/4 min-h-screen fixed overflow-y-auto sidemenu ${menuVisible ? "" : "hidden"
+            className={`bg-sky-950 w-1/5 min-h-screen fixed overflow-y-auto p-6 sidemenu ${menuVisible ? "" : "hidden"
                 }`}
         >
             <div className="container mx-auto text-white">
