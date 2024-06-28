0037dbfc2d610ae08df62488562302d1156b15fb
rin.notepc <nekosuki.works@gmail.com>
2024-06-28 12:39:29 +0900

    ホットリロード対応

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+const host = "localhost";
 export default defineConfig({
+    server: {
+        hmr: { host },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
