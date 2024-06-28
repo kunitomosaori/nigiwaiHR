7202a026f25bc87f30b48c93e1647f52c79c1d92
rin.notepc <nekosuki.works@gmail.com>
2024-06-28 12:30:07 +0900

    SideMenuの見た目を修正。

--- resources/js/Components/SideMenu.jsx
+++ resources/js/Components/SideMenu.jsx
@@ -1,12 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@inertiajs/react";
-
+import { TbInbox, TbChevronDown, TbChevronRight } from "react-icons/tb";
+const treeData = [
+    {
+        title: '受信トレイ',
+        key: '0-0',
+        icon: <TbInbox className="text-2xl" />,
+        link: '/dashboard',
+        children: [
+            {
+                title: '目標記入',
+                key: '0-0-0',
+                link: '/dashboard',
+            },
+            {
+                title: '振り返り',
+                key: '0-0-1',
+                link: '/dashboard',
+            },
+            {
+                title: '承認',
+                key: '0-0-2',
+                link: '/dashboard',
+            },
+        ],
+    },
+    {
+        title: '評価シート作成',
+        key: '0-0',
+        icon: <TbInbox className="text-2xl" />,
+        link: '/dashboard',
+    },
+    {
+        title: '期初',
+        key: '0-1',
+        link: '/idl/index_idl',
+    },
+    {
+        title: '期末',
+        key: '0-2',
+        link: '/idl/select_idl',
+    },
+    {
+        title: 'ログアウト',
+        key: '0-3',
+        link: '/phpkadai05/logout',
+    },
+];
+const TreeNode = ({ node }) => {
+    const [expanded, setExpanded] = useState(false);
+    const handleExpand = () => {
+        setExpanded(!expanded);
+    };
+    return (
+        <li className="border-b md:border-none flex flex-col hover:bg-sky-700 rounded">
+            <div className="flex items-center">
+                {node.children && (
+                    <span onClick={handleExpand} className="cursor-pointer">
+                        {expanded ? <TbChevronDown className="text-2xl" /> : <TbChevronRight className="text-2xl" />}
+                    </span>
+                )}
+                {node.icon && node.icon}
+                <Link href={node.link} className="block px-6 py-1 my-2">
+                    {node.title}
+                </Link>
+            </div>
+            {expanded && node.children && (
+                <ul className="ml-4">
+                    {node.children.map((childNode) => (
+                        <TreeNode key={childNode.key} node={childNode} />
+                    ))}
+                </ul>
+            )}
+        </li>
+    );
+};
 const SideMenu = ({ menuVisible }) => {
     return (
         <div
-            className={`bg-sky-950 w-1/4 min-h-screen fixed overflow-y-auto sidemenu ${
-                menuVisible ? "" : "hidden"
-            }`}
+            className={`bg-sky-950 w-1/4 min-h-screen fixed overflow-y-auto sidemenu ${menuVisible ? "" : "hidden"
+                }`}
         >
             <div className="container mx-auto text-white">
                 <div className="flex justify-between items-center">
@@ -14,133 +87,13 @@ const SideMenu = ({ menuVisible }) => {
                 </div>
                 <div id="menu" className="md:flex">
                     <ul className="flex flex-col">
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/nigiwai-dashboard"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                ダッシュボード
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/idl/index_idl"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                期初
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/idl/select_idl"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                期末
-                            </Link>
-                        </li>
-                        {/* <li className="border-b md:border-none">
-                            <Link
-                                href="/sheet-register"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                人事評価シート登録
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/phpkadai05/select"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                人事評価シート一覧
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/user-register"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                ユーザー登録
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/phpkadai05/company/index_co"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                会社目標登録
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/phpkadai05/company/select_co"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                会社目標表示
-                            </Link>
-                        </li> */}
-                        {/* <li className="border-b md:border-none">
-                            <Link
-                                href="/idl/index_idl"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                個人目標登録
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/idl/select_idl"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                個人目標表示
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/phpkadai05/goal/index_goal"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                業績目標登録
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/phpkadai05/goal/select_goal"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                業績目標表示
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/phpkadai05/competency/index_competency"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                行動評価登録
-                            </Link>
-                        </li>
-                        <li className="border-b md:border-none">
-                            <Link
-                                href="/phpkadai05/competency/select_competency"
-                                className="block px-8 py-2 my-4 hover:bg-sky-700 rounded"
-                            >
-                                行動評価表示
-                            </Link>
-                        </li> */}
-                        <li>
-                            <div className="my-8 text-center md:my-4">
-                                <Link
-                                    href="/phpkadai05/logout"
-                                    className="px-6 py-2 bg-orange-500 hover:bg-orange-400 rounded-full"
-                                >
-                                    ログアウト
-                                </Link>
-                            </div>
-                        </li>
+                        {treeData.map((node) => (
+                            <TreeNode key={node.key} node={node} />
+                        ))}
                     </ul>
                 </div>
             </div>
         </div>
     );
 };
-
 export default SideMenu;
